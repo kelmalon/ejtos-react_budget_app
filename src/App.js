3d29ae9e7c7dc9e94bd05fecb197e3af4649c5a9
kelmalon <kel.malon.694@gmail.com>
2024-06-21 18:29:08 -0400

    completed project

--- src/App.js
+++ src/App.js
@@ -25,6 +25,9 @@ const App = () => {
                     <div className='col-sm'>
                         <ExpenseTotal />
                     </div>
+                    <div className = 'selector'>
+                        <CurrencySelector/>
+                    </div>
                 </div>
                 <h3 className='mt-3'>Allocation</h3>
                 <div className='row '>
@@ -36,9 +39,6 @@ const App = () => {
                 <div className='row mt-3'>
                     <div className='col-sm'>
                         <AllocationForm/>
-                    </div>
-                    <div className = 'selector'>
-                        <CurrencySelector/>
                     </div>
                 </div>
             </div>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -27,11 +27,25 @@ const ExpenseItem = (props) => {
 
     }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: -10,
+        };
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+
+    }
+
     return (
         <tr>
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><button className = 'increase' onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><button className = 'decrease' onClick={event=> decreaseAllocation(props.name)}>-</button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
